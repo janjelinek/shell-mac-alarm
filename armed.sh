4111a6b6c033bda55527d8c5a69fb801ce0423ab
Jan Jelinek <kurnick@gmail.com>
2020-08-29 16:37:58 +0200

    Fix path when running file from other then repo dir

--- armed.sh
+++ armed.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 alaramOn=false;
+BASEDIR=$(dirname "$0");
 while true; do
     if $(pmset -g batt | head -n 1 | grep -q "Battery"); then
         blueutil --power 0;
         osascript -e 'set volume output volume 100';
         osascript -e 'set volume output muted false';
         say "Get your hands off";
-        afplay -v 50 alarm.aiff -t 2;
+        afplay -v 50 $BASEDIR/alarm.aiff -t 2;
         alaramOn=true;
     else
         alaramOn=false;
